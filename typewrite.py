4e3b176e39d1adb23b2ef09bff4e4daeb94db375
Frank Seifferth <frankseifferth@posteo.net>
2022-06-26 17:20:50 +0200

    Add explanation to help message

--- typewrite.py
+++ typewrite.py
@@ -82,6 +82,18 @@ def typewrite(filename: str):
 
 _cli_help = """
 Usage: typewrite <INPUT.TXT> <OUTPUT.PDF>
+
+Produce a pdf version of a plain text file with a typewritten look.
+
+The original plain text version will be included as the first object
+in the pdf file and will not be used for anything. This way the pdf
+file can simply be opened in a text editor in order to extract the
+original plain text version located at the top of the file.
+
+Typewrite processes form-feed characters to adjust page breaks
+accordingly. In all other cases it simply breaks pages after 57 lines
+of text. It is therefore possible to use old-school page formatting
+commands like 'pr --length=57' to prepare text for typewrite.
 """.lstrip()
 
 if __name__ == "__main__":
