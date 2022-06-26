c41139763262e2d60633cadaf94c4011dd2804c8
Frank Seifferth <frankseifferth@posteo.net>
2022-06-26 16:29:41 +0200

    Use plain padding in latex input
    Using a standalone object for padding causes the xref table to
    contain a broken entry. This can be avoided adding a single object
    for plain padding.

--- typewrite.py
+++ typewrite.py
@@ -38,19 +38,14 @@ _texformfeed = r"""
 _textfilehead = r"""
 \bgroup
   \pdfvariable objcompresslevel=0
-  \immediate\pdfextension obj file {
+  \immediate\pdfextension obj {
 """.strip()
 _textfilefoot = r"""
   }
 \egroup
-\immediate\pdfextension obj {
-This is some placeholder text that will be removed later on. Its purpose
-is to provide some padding that can be used in post-processing the pdf
-file later on.
-}
 """.lstrip()
-_textobjend = b"""
-\nendstream\nendobj\n
+_textstreamend = b"""
+\n\nendstream\nendobj\n
 % This is the end of the file content's plain text version. What follows
 % are mostly binary blobs used for displaying this file as pdf.
 """
@@ -58,10 +53,15 @@ _textobjend = b"""
 def typewrite(filename: str):
     with open(filename) as f:
         text = f.read()
+        textlength = len(text.encode('utf-8'))
+        streamhead = f'<< /Length {textlength+3} >>\nstream\n\n' \
+                                                    .encode('utf-8')
     with TemporaryDirectory() as outdir:
         with open(f'{outdir}/document.tex', 'w') as f:
             f.write(_textfilehead)
-            f.write(filename)
+            f.write(                                        # Add padding
+                (len(streamhead)+textlength+len(_textstreamend)-7) * 'x'
+            )
             f.write(_textfilefoot)
             f.write(_texhead)
             f.write(text.replace('\f', _texformfeed))
@@ -73,11 +73,12 @@ def typewrite(filename: str):
                        text=True, check=True)
         with open(f'{outdir}/document.pdf', 'rb') as f:
             pdf = f.read()
-        textlength = len(text.encode('utf-8')) + 183
         textoffset = pdf.find(b'\n1 0 obj\n') + 9
-        streamhead = f'<< /Length {textlength} >>\nstream\n'.encode('utf-8')
+        objoffset = textoffset + len(streamhead) + len(_textstreamend) + \
+                    textlength
         return pdf[:textoffset] + streamhead + text.encode('utf-8') + \
-               _textobjend + pdf[textoffset+textlength:]
+               _textstreamend + \
+               pdf[objoffset:]
 
 _cli_help = """
 Usage: typewrite <INPUT.TXT> <OUTPUT.PDF>
